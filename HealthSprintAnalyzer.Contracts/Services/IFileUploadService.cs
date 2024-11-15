0d1be41e161d0f018c479a06ba3f680ef4fb1515
JordennJd <jordenn228@gmail.com>
2024-11-16 02:51:01 +0300

    Revert "first commit"
    This reverts commit f38bd4da7a5426d841af3ffa7c9df47e5e6226cb.

--- HealthSprintAnalyzer.Contracts/Services/IFileUploadService.cs
+++
@@ -1,6 +0,0 @@
-namespace HealthSprintAnalyzer.Contracts.Services;
-
-public interface IFileUploadService
-{
-	Task UploadFileAsync(Stream sprintFile, Stream ticketFile, Stream ticketHistoryFile);
-}
